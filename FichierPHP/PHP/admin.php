5e9518aa643561c0cb6f94072c5a322b0ff5733e
Ababacar Sadikh GUEYE <49489598+Sadikh-SA@users.noreply.github.com>
2019-05-13 10:31:15 +0000

    Update admin.php

--- FichierPHP/PHP/admin.php
+++ FichierPHP/PHP/admin.php
@@ -2,7 +2,7 @@
     <?php include("header1.php");
 ?>
 <section class="container">
-<h1 style="text-align:center; color: darkgreen;">Liste des Utilisateurs</h1>
+<h1 style="text-align:center; box-shadow: 12px 3px 12px 3px; border-radius: 12px 12px; color: darkgreen; width:25%; border:1px solid green; margin: 0 auto; margin-bottom: 20px;">Liste des Utilisateurs</h1>
 <?php
 //tableau
 echo  '<table class="table table-dark table-striped">
@@ -19,7 +19,35 @@ echo  '<table class="table table-dark table-striped">
               </thead>
               <tbody>';
 
-              $id_file= fopen("../Fichiers/utilisateur.txt","r");
+              if (isset($_POST['statut'])) {
+                $id_file= fopen("../Fichiers/utilisateur.txt","r");
+                $login=$_GET['login'];
+                while($ligne=fgets($id_file)){
+                    $tab = explode(",",$ligne);
+                    echo "<tr>";
+                    if ($tab[0]==$login) {
+                        for ($i=0; $i <count($tab) ; $i++) { 
+                            if ($tab[6]=="Bloquer\n" || $tab[6]=="Bloquer") {
+                                if ($i==(count($tab)-1)) {
+                                    echo "<td><a href='modif.php?login=".$tab[0]."'><button type='submit' class='btn btn-danger' name='statut' style='width:80%; text-align:center;'>Actif</button></a></td>";
+                                } else {
+                                    echo "<td class='bg-gradient-secondary';>" .$tab[$i]. "</td>";
+                                }
+                            }
+                            else {
+                                if ($i==(count($produit)-1)) {
+                                    $tab[6]=="Bloquer";
+                                    echo "<td><a href='modif.php?login=".$tab[0]."'><button type='submit' class='btn btn-success' name='statut' style='width:80%; text-align:center;'>Bloquer</button></a></td>";
+                                } else {
+                                    echo "<td class='.bg-gradient-secondary'>" .$tab[$i]. "</td>";
+                                }
+                            }
+                        }
+                    }
+                    
+                }
+              } else {
+                $id_file= fopen("../Fichiers/utilisateur.txt","r");
                 $j=0;
                // $chaine ="Bloquer";
                 while($ligne=fgets($id_file)){
@@ -28,14 +56,14 @@ echo  '<table class="table table-dark table-striped">
                     for ($i=0; $i <count($produit) ; $i++) { 
                         if ($produit[6]=="Bloquer\n" || $produit[6]=="Bloquer") {
                             if ($i==(count($produit)-1)) {
-                                echo "<td><form action='admin.php' method='POST'><button type='submit' class='btn btn-danger' name='statut' style='width:80%; text-align:center;'>".$produit[6]."</button></form></td>";
+                                echo "<td><a href='modif.php?login=".$produit[0]."'><button type='submit' class='btn btn-danger' name='statut' style='width:80%; text-align:center;'>".$produit[6]."</button></a></td>";
                             } else {
                                 echo "<td class='bg-gradient-secondary';>" .$produit[$i]. "</td>";
                             }
                         }
                         else {
                             if ($i==(count($produit)-1)) {
-                                echo "<td><form action='admin.php' method='POST'><button type='submit' class='btn btn-success' name='statut' style='width:80%; text-align:center;'>".$produit[6]."</button></form></td>";
+                                echo "<td><a href='modif.php?login=".$produit[0]."'><button type='submit' class='btn btn-success' name='statut' style='width:80%; text-align:center;'>".$produit[6]."</button></a></td>";
                             } else {
                                 echo "<td class='.bg-gradient-secondary'>" .$produit[$i]. "</td>";
                             }
@@ -44,23 +72,8 @@ echo  '<table class="table table-dark table-striped">
                     echo "</tr>";
                 }
                 fclose($id_file);
-
-                $file= fopen("../Fichiers/utilisateur.txt","a+");
-                if (isset($_POST['statut'])) {
-                    while($ligne=fgets($file)){
-                        $tab = explode(",",$ligne);
-                        echo "<tr>";
-                        for ($i=0; $i <count($tab) ; $i++) { 
-                            if ($tab[6]=="Bloquer") {
-                                
-                            }
-                            else {
-                                
-                            }
-                        }
-                        echo "</tr>";
-                    }
-                }
+              }
+              
                 echo "</table>";
 ?>
 </section>
